f223a33de27fe1286b96ca7d86c51472fe19d6f9
LuisRSSx <luis_ricardo1996live.com@live.com>
2025-05-25 20:06:25 -0300

    sequelize_1

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+const express = require('express');
+const app = express();
 const sequelize = require('./src/config/database');
+const productRoutes = require('./src/routes/productRoutes');
+
+app.use(express.json());
+
+app.use('/api/products', productRoutes);
+
 
 sequelize.sync({ force: false }) // force: true apaga e recria tabelas (cuidado!)
   .then(() => console.log('Banco de dados conectado!'))
---
+++ src/controllers/ProductController.js
@@ -0,0 +1,19 @@
+const Product = require('../models/Product');
+
+exports.create = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.list = async (req, res) => {
+  try {
+    const products = await Product.findAll();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
